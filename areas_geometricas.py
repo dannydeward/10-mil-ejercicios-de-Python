8eaa9aef2ddfb87a28a97534ab09aaaa7c0ecd69
Danny Ramirez <58273239+dannydeward@users.noreply.github.com>
2023-12-05 13:10:21 -0300

    Add files via upload
    aquí tenemos los primeros 3 ejercicios de Python calculo del área del triangulo, rectángulo y cuadrado , ademas anexe una pagina con ejercicios boléanos

---
+++ areas_geometricas.py
@@ -0,0 +1,21 @@
+#area del cuadro es igual a lx l 
+# area del triuangulo es igual a bxh/2
+#area del rectangulo es igual BxA
+
+"""lado=int(input('ingrese el valor del lado del cuadrado:' ))
+area_cuadro= lado*lado
+print('el area del cuadro es:', area_cuadro)"""
+
+"""base= int(input('ingrese el valor de la base del triangulo:' ))
+altura=int(input('ingrese el valor de la altura del triangulo:' ))
+
+area_triangulo= (base * altura)/2
+
+print('el area del triangulo es:', area_triangulo)"""
+
+base= int(input('ingrese el valor de la base del rectangulo:' ))
+altura=int(input('ingrese el valor de la altura del rectangulo:' ))
+
+area_rectangulo= base * altura
+
+print('esta es el area del rectangulo:', area_rectangulo)
---
+++ ejercicios.py
@@ -0,0 +1,9 @@
+a=int(input('ingresar un numero entero:' ))
+b=int(input('ingresar otro numero entero:' ))
+print(a<b)
+print(a==b)
+c= 2*a
+d=b/2
+print(b>d)
+print(d==a)
+print(c>b and c>a)
