9a75667b14adaa4589c1a5c6404e60e9a839b25d
Danny Ramirez <58273239+dannydeward@users.noreply.github.com>
2024-01-04 09:24:39 -0300

    Add files via upload
    actualizacion con mas areas geometricas

--- areas_geometricas.py
+++ areas_geometricas.py
@@ -2,20 +2,51 @@
 # area del triuangulo es igual a bxh/2
 #area del rectangulo es igual BxA
 
+#AREA DEL CUADRADO 1
 """lado=int(input('ingrese el valor del lado del cuadrado:' ))
 area_cuadro= lado*lado
-print('el area del cuadro es:', area_cuadro)"""
+print('el area del cuadro es:', area_cuadro)
 
-"""base= int(input('ingrese el valor de la base del triangulo:' ))
+
+#AREA DEL TRIANGULO 2
+base= int(input('ingrese el valor de la base del triangulo:' ))
 altura=int(input('ingrese el valor de la altura del triangulo:' ))
 
 area_triangulo= (base * altura)/2
 
-print('el area del triangulo es:', area_triangulo)"""
+print('el area del triangulo es:', area_triangulo)
 
+
+#AREA DEL RECTANGULO 3
 base= int(input('ingrese el valor de la base del rectangulo:' ))
 altura=int(input('ingrese el valor de la altura del rectangulo:' ))
 
 area_rectangulo= base * altura
 
-print('esta es el area del rectangulo:', area_rectangulo)
+print('esta es el area del rectangulo:', area_rectangulo)"""
+
+
+"""AREA DEL ROMBO
+D=int(input("ingrese en valor de la diagonal mayor:" ))
+d=int(input("ingrese en valor de la diagonal menor:" ))
+
+Area= D * d 
+
+print("el area del Rombo es.", Area)"""
+
+"""AREA DEL TRAPECIO
+h= int(input("Ingrese la altura del Trapecio."  ))
+B= int(input("Ingrese la Base Mayor del Trapecio."  ))
+b= int(input("Ingrese la Base Menor del Trapecio."  ))
+
+Area =((h*(B*b)))/2
+
+print("el area del Trapecio es:", Area)"""
+
+"""#Area del Circulo 
+pi=13.1416
+r=int(input("Ingrese el radio de la circunferencia:"  )) 
+
+Area= pi*(r**2)
+
+print("el area de la circunferencia es:", Area)"""
